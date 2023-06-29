8fcfeac7768b89b6ba8ebd98fac02a7562a69130
1evanter <rita.oleksenkoo@gmail.com>
2023-06-29 22:19:23 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from '/node_modules/lodash.throttle';
 
+  const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(setPlayTime, 1000));
+
+function setPlayTime(evt) {
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(evt.seconds))
+}
+    
+const currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? [];
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+          
+        break;
+
+        default:
+      
+        break;
+    }
+});
